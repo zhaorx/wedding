6b0e19e09b472cf1e9d5c474b17deee3b459932a
zhaorx <547070625@qq.com>
2018-03-11 22:35:57 +0800

    微信回调uri更改

--- config/weixin.js
+++ config/weixin.js
@@ -2,5 +2,5 @@ module.exports.weixin = {
   // 微信公众号相关设置
   appid: 'wx9db84d33fd2c2d88',
   secret: 'd4624c36b6795d1d99dcf0547af5443d',
-  redirectUri: 'http://chengchen.vicp.cc:34604/home/resolve'
+  redirectUri: 'http://139.196.126.164:1337/home/resolve'
 };
