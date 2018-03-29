419563ef768ae9bcabb69ea1f61856015a990a26
zhaorx <547070625@qq.com>
2018-03-29 21:09:12 +0800

    、、

--- config/weixin.js
+++ config/weixin.js
@@ -1,7 +1,9 @@
 module.exports.weixin = {
   // 微信公众号相关设置
+  //测试号
   appid: 'wx9db84d33fd2c2d88',
   secret: 'd4624c36b6795d1d99dcf0547af5443d',
   // redirectUri: 'http://139.196.126.164/home/resolve'
-  redirectUri: 'http://zrx.ht-dep.club//home/resolve'
+  // redirectUri: 'http://chengchen.vicp.cc:34604/home/resolve'
+  redirectUri: 'http://zrx.ht-dep.club/home/resolve'
 };
