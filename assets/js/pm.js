2778a13e8173512843b5daf2bcc8448b562f3684
zhaorx <547070625@qq.com>
2018-04-02 19:45:00 +0800

    修复BGM无法自动播放

--- assets/js/pm.js
+++ assets/js/pm.js
@@ -8,15 +8,15 @@
     var el = document.createElement('bootstrap');
 
     var transEndEventNames = {
-      WebkitTransition : 'webkitTransitionEnd',
-      MozTransition    : 'transitionend',
-      OTransition      : 'oTransitionEnd otransitionend',
-      transition       : 'transitionend'
+      WebkitTransition: 'webkitTransitionEnd',
+      MozTransition: 'transitionend',
+      OTransition: 'oTransitionEnd otransitionend',
+      transition: 'transitionend'
     }
 
     for (var name in transEndEventNames) {
       if (el.style[name] !== undefined) {
-        return { end: transEndEventNames[name] };
+        return {end: transEndEventNames[name]};
       }
     }
 
@@ -27,8 +27,12 @@
   $.fn.emulateTransitionEnd = function (duration) {
     var called = false;
     var $el = this;
-    $(this).one('bsTransitionEnd', function () { called = true; });
-    var callback = function () { if (!called) $($el).trigger($.support.transition.end); };
+    $(this).one('bsTransitionEnd', function () {
+      called = true;
+    });
+    var callback = function () {
+      if (!called) $($el).trigger($.support.transition.end);
+    };
     setTimeout(callback, duration);
     return this;
   };
@@ -50,25 +54,25 @@
 }(jQuery));
 
 
-(function($) {
+(function ($) {
   /**
    * textarea自适应内容高度
    */
-  $.fn.fittextarearows = function(minRows, maxRows) {
-    return this.each(function() {
-      if(this.tagName == 'TEXTAREA') {
-        $(this).on('keyup.fitrows input.fitrows blur.fitrows', function() {
+  $.fn.fittextarearows = function (minRows, maxRows) {
+    return this.each(function () {
+      if (this.tagName == 'TEXTAREA') {
+        $(this).on('keyup.fitrows input.fitrows blur.fitrows', function () {
           var rows = minRows || 2, $t = $(this);
           maxRows = maxRows || 5;
-          while(rows <= maxRows) {
+          while (rows <= maxRows) {
             $t.attr('rows', rows).css('overflow', 'hidden');
-            if($t.innerHeight() < $t[0].scrollHeight) {
+            if ($t.innerHeight() < $t[0].scrollHeight) {
               rows++;
             } else {
               break;
             }
           }
-          if(rows > maxRows) {
+          if (rows > maxRows) {
             $t.css('overflow', 'auto');
           }
         }).trigger('keyup.fitrows');
@@ -78,12 +82,12 @@
 })(jQuery);
 
 
-(function($, win) {
+(function ($, win) {
   // 全局PM对象
   var PM = {};
   win.PM = PM;
 
-  win.requestAnimationFrame =  win.requestAnimationFrame ||
+  win.requestAnimationFrame = win.requestAnimationFrame ||
     win.mozRequestAnimationFrame ||
     win.webkitRequestAnimationFrame ||
     win.msRequestAnimationFrame;
@@ -99,13 +103,13 @@
   function Poller(looper, interval, times) {
     var self = this, tm = null, count = 0;
 
-    this.looper = function() {
-      return $.Deferred(function(dtd) {
+    this.looper = function () {
+      return $.Deferred(function (dtd) {
         var res = looper.call(self);
 
         if (res && typeof res.then == 'function') {
-          res.always(function() {
-              dtd.resolve();
+          res.always(function () {
+            dtd.resolve();
           });
         } else {
           dtd.resolve();
@@ -119,58 +123,58 @@
     this.id = +new Date();
     this.sleeping = false;
 
-    this.poll = function() {
-      this.looper().done(function() {
+    this.poll = function () {
+      this.looper().done(function () {
         if (++count < self.times) {
           self.start();
         }
       });
     };
 
-    this.start = function() {
-      tm = setTimeout(function() {
+    this.start = function () {
+      tm = setTimeout(function () {
         self.poll();
       }, this.interval);
 
       return this;
     };
 
-    this.pause = function() {
+    this.pause = function () {
       clearTimeout(tm);
 
       return this;
     };
 
-    this.autoSleep = function(timeoutToSleep) {
+    this.autoSleep = function (timeoutToSleep) {
       timeoutToSleep = timeoutToSleep || 60000;
       var activeTm;
       // go to sleep, when user mouse is not active on page until timeout
-      $(document).off('mousemove.poller-active' + this.id).on('mousemove.poller-active' + this.id, function() {
+      $(document).off('mousemove.poller-active' + this.id).on('mousemove.poller-active' + this.id, function () {
         activeTm && clearTimeout(activeTm);
-        activeTm = setTimeout(function() {
-            self.sleep();
+        activeTm = setTimeout(function () {
+          self.sleep();
         }, timeoutToSleep);
       }).triggerHandler('mousemove.poller-active' + this.id);
       // go to sleep immediately, when user window lose focus
-      $(win).off('blur.poller-active' + this.id).on('blur.poller-active' + this.id, function() {
+      $(win).off('blur.poller-active' + this.id).on('blur.poller-active' + this.id, function () {
         self.sleep();
       });
 
       return this;
     };
 
-    this.sleep = function() {
+    this.sleep = function () {
       if (this.sleeping) {
         return;
       }
 
       this.sleeping = true;
       // wakeup while mouse move
-      $(document).one('mousemove.poller-sleep' + this.id, function() {
+      $(document).one('mousemove.poller-sleep' + this.id, function () {
         self.wakeup();
       });
       // wakeup while window get focus
-      $(win).one('focus.poller-sleep' + this.id, function() {
+      $(win).one('focus.poller-sleep' + this.id, function () {
         self.wakeup();
       });
       this.interval = this.interval * 8;
@@ -178,7 +182,7 @@
       return this;
     };
 
-    this.wakeup = function() {
+    this.wakeup = function () {
       if (!this.sleeping) {
         return;
       }
@@ -197,8 +201,8 @@
   /**
    * 与后台api交互的简单封装
    */
-  PM.request = function(opt) {
-    return $.ajax(opt).pipe(function(r) {
+  PM.request = function (opt) {
+    return $.ajax(opt).pipe(function (r) {
       if (r.status == 'failed') {
         return $.Deferred().reject(r.error);
       } else {
@@ -210,24 +214,24 @@
   /**
    * 提示框
    */
-  PM.toast = function(msg, delay) {
+  PM.toast = function (msg, delay) {
     msg = msg || '已完成';
     delay = delay || 3000;
     var $toast = $(
       '<div id="toast" style="display: none;">' +
-        '<div class="weui_mask_transparent"></div>' +
-        '<div class="weui_toast">' +
-          '<i class="weui_icon_toast"></i>' +
-          '<p class="weui_toast_content">' + msg + '</p>' +
-        '</div>' +
+      '<div class="weui_mask_transparent"></div>' +
+      '<div class="weui_toast">' +
+      '<i class="weui_icon_toast"></i>' +
+      '<p class="weui_toast_content">' + msg + '</p>' +
+      '</div>' +
       '</div>'
     );
 
-    $toast.appendTo('body').show().on('tap', function(e) {
+    $toast.appendTo('body').show().on('tap', function (e) {
       $toast.remove();
       e.preventDefault();
     });
-    setTimeout(function() {
+    setTimeout(function () {
       $toast.remove();
     }, delay);
   };
@@ -256,7 +260,7 @@
     this.init();
   }
 
-  Tilt.prototype.init = function() {
+  Tilt.prototype.init = function () {
     this.initViewport();
     var renderHeight = Math.min(this.viewport.height, this.options.height);
     var renderWidth = this.aspectRatio * renderHeight;
@@ -266,21 +270,21 @@
     this.reset();
   };
 
-  Tilt.prototype.reset = function() {
+  Tilt.prototype.reset = function () {
     this.setX(-this.leftOffset);
     return this;
   };
 
-  Tilt.prototype.setX = function(x) {
+  Tilt.prototype.setX = function (x) {
     this.$el.css('transform', 'translateX(' + Math.round(x) + 'px)');
   };
 
-  Tilt.prototype.initViewport = function() {
+  Tilt.prototype.initViewport = function () {
     this.viewport.width = this.options.container.width();
     this.viewport.height = this.options.container.height();
   };
 
-  Tilt.prototype.update = function(tilt) {
+  Tilt.prototype.update = function (tilt) {
     var maxOffset = Math.max(this.leftOffset, this.rightOffset);
     var leftMaxTilt = this.leftOffset * this.options.maxTilt / maxOffset;
     var rightMaxTilt = this.rightOffset * this.options.maxTilt / maxOffset;
@@ -302,44 +306,44 @@
     lastTilt: 0,
     enabled: true,
     stoped: false,
-    init: function() {
+    init: function () {
       var self = this;
       if (win.DeviceOrientationEvent) {
         var averageGamma = [];
-        win.addEventListener('deviceorientation', function(e) {
+        win.addEventListener('deviceorientation', function (e) {
           if (self.enabled && !self.stoped) {
             if (averageGamma.length > 8) {
               averageGamma.shift();
             }
             averageGamma.push(e.gamma);
-            self.lastTilt = averageGamma.reduce(function(a, b) {
-              return a+b;
-            }) / averageGamma.length;
+            self.lastTilt = averageGamma.reduce(function (a, b) {
+                return a + b;
+              }) / averageGamma.length;
           }
         }, false);
         win.requestAnimationFrame($.proxy(self.update, self));
       }
     },
-    update: function() {
+    update: function () {
       if (this.tiltObj) {
         this.tiltObj.update(this.lastTilt);
         win.requestAnimationFrame($.proxy(this.update, this));
       }
     },
-    setTilt: function(to) {
+    setTilt: function (to) {
       this.tiltObj = to;
       return this;
     },
-    pause: function() {
+    pause: function () {
       this.stoped = true;
     },
-    play: function() {
+    play: function () {
       this.stoped = false;
     },
-    enable: function() {
+    enable: function () {
       this.enabled = true;
     },
-    disable: function() {
+    disable: function () {
       this.enabled = false;
       this.lastTilt = 0;
     }
@@ -350,13 +354,13 @@
    */
   PM.map = {
     dtd: $.Deferred(),
-    load: function() {
+    load: function () {
       var self = this;
-      setTimeout(function() {
+      setTimeout(function () {
         self.dtd.resolve();
       }, 100);
     },
-    open: function() {
+    open: function () {
       if (PM.map.dtd.state() == 'resolved') {
         return $.Deferred().resolve();
       } else {
@@ -365,12 +369,12 @@
           url: 'http://webapi.amap.com/maps?v=1.3&key=ae7ec99e2aa1832237f39584e0b4418f&callback=gd_map_load',
           dataType: 'script',
           cache: true
-        }).then(function() {
+        }).then(function () {
           return PM.map.dtd;
-        }).done(function() {
-          var map = new AMap.Map('pnl-map',{
+        }).done(function () {
+          var map = new AMap.Map('pnl-map', {
             zoom: 20,
-            center: [118.519766,37.472175]
+            center: [118.519766, 37.472175]
           });
           var marker = new AMap.Marker({
             position: map.getCenter()
@@ -382,7 +386,7 @@
             offset: new AMap.Pixel(-75, -30),
             content: "我们的婚礼于04月21日中午"
           });
-        }).fail(function() {
+        }).fail(function () {
 
         });
       }
@@ -408,22 +412,22 @@
     this.init();
   }
 
-  PullDown.prototype.init = function() {
+  PullDown.prototype.init = function () {
     this.initEvent();
   };
 
-  PullDown.prototype.initEvent = function() {
+  PullDown.prototype.initEvent = function () {
     var self = this;
     var touchstartTime;
     var startY = 0;
     var changedY = 0;
     var startPadding = 20;
-    this.$handler.on('touchstart', function(e) {
+    this.$handler.on('touchstart', function (e) {
       self.$el.addClass('disable-anim');
       startY = e.originalEvent.touches[0].clientY;
       touchstartTime = new Date().getTime();
       self.setY(self.$el.hasClass('expand') ? self.originY : self.originY + startPadding);
-    }).on('touchmove', function(e) {
+    }).on('touchmove', function (e) {
       changedY = e.originalEvent.touches[0].clientY - startY;
       if (!self.$el.hasClass('expand') && Math.abs(changedY) < startPadding) {
         return;
@@ -439,53 +443,53 @@
         y = Math.min(0, Math.max(y, -self.height));
         self.setY(y);
       }
-    }).on('touchend', function(e) {
-      var pos = changedY > 0 ? 2/3 : 1/3;
+    }).on('touchend', function (e) {
+      var pos = changedY > 0 ? 2 / 3 : 1 / 3;
       if (self.currentY >= -pos * self.height) {
         self.down();
       } else if (self.currentY < -pos * self.height) {
         self.up();
       }
-    }).on('click', function() {
+    }).on('click', function () {
       if (self.$el.hasClass('expand')) {
         self.up();
       }
     });
 
-    this.$el.on('click', function() {
+    this.$el.on('click', function () {
       if (self.$el.hasClass('expand')) {
         self.up();
       }
     });
   };
 
-  PullDown.prototype.down = function() {
+  PullDown.prototype.down = function () {
     this.$el.removeClass('disable-anim').addClass('expand').removeClass('closed');
     this.setY(0);
     this.originY = this.currentY;
   };
 
-  PullDown.prototype.up = function() {
+  PullDown.prototype.up = function () {
     this.$el.removeClass('disable-anim').removeClass('expand');
     this.setY(-this.height);
     this.originY = this.currentY;
     this.options.onClose && this.options.onClose.call(this);
   };
 
-  PullDown.prototype.setY = function(y) {
+  PullDown.prototype.setY = function (y) {
     this.currentY = y;
     this.$el.css('transform', 'translateY(' + this.currentY + 'px)');
   };
 
   PM.pull = new PullDown($('#about-box'), {
     handler: $('#btn-pull-down'),
-    onClose: function() {
+    onClose: function () {
       var self = this;
       if (self.$el.data('noAbout')) {
         return;
       }
 
-      PM.request({url: '/home/api_closeAboutUs'}).then(function() {
+      PM.request({url: '/home/api_closeAboutUs'}).then(function () {
         self.$el.data('noAbout', true);
       });
     }
@@ -518,24 +522,37 @@
     this.$btn.append(this.$audio);
     this.initEvent();
 
+    var self = this;
     if (this.options.autoplay) {
+      if (window.WeixinJSBridge) {
+        WeixinJSBridge.invoke('getNetworkType', {}, function (e) {
+          self.play();
+        }, false);
+      } else {
+        document.addEventListener("WeixinJSBridgeReady", function () {
+          WeixinJSBridge.invoke('getNetworkType', {}, function (e) {
+            self.play();
+          });
+        }, false);
+      }
       this.play();
     } else {
       this.pause();
+
     }
   }
 
-  BGM.prototype.initEvent = function() {
+  BGM.prototype.initEvent = function () {
     var self = this;
-    this.$audio.on("canplay", function() {
+    this.$audio.on("canplay", function () {
       return self.$audio.get(0).play();
-    }).on("play", function() {
+    }).on("play", function () {
       return self.$btn.addClass("playing");
-    }).on("pause", function() {
+    }).on("pause", function () {
       return self.$btn.removeClass("playing");
     });
 
-    this.$btn.on('tap', function(e) {
+    this.$btn.on('tap', function (e) {
       e.preventDefault();
       if (self.$audio.get(0).paused) {
         self.play();
@@ -545,11 +562,11 @@
     });
   };
 
-  BGM.prototype.play = function() {
+  BGM.prototype.play = function () {
     this.$audio.get(0).play();
   };
 
-  BGM.prototype.pause = function() {
+  BGM.prototype.pause = function () {
     this.$audio.get(0).pause();
   };
 
@@ -561,15 +578,15 @@
    */
   PM.lastFeedID = 0;
   PM.maxFeeds = 20;
-  PM.poller = new Poller(function() {
+  PM.poller = new Poller(function () {
     return PM.request({
-        'url': '/home/api_lastFeeds',
-        'cache': false,
-        'data': {
-            'lastID': PM.lastFeedID,
-            'limit': PM.maxFeeds
-        },
-    }).done(function(d) {
+      'url': '/home/api_lastFeeds',
+      'cache': false,
+      'data': {
+        'lastID': PM.lastFeedID,
+        'limit': PM.maxFeeds
+      },
+    }).done(function (d) {
       if (d && d.length > 0) {
         PM.lastFeedID = d[0].id;
         PM.board.unshift(d.reverse());
@@ -607,30 +624,30 @@
     duration: 800,
     showName: false,
     pendingMsg: [],
-    init: function($el) {
+    init: function ($el) {
       this._cm = new CommentManager($el.get(0));
       this._cm.init();
       this.play();
     },
-    push: function(ws) {
+    push: function (ws) {
       this._wishes = this._wishes.concat(ws);
     },
-    unshift: function(ws) {
+    unshift: function (ws) {
       if (!$.isArray(ws)) {
         ws = [ws];
       }
       this._wishes = ws.concat(this._wishes);
     },
-    pause: function() {
+    pause: function () {
       this._stopped = true;
       this._cm.stop();
     },
-    play: function() {
+    play: function () {
       this._stopped = false;
       this._cm.start();
       this.loop();
     },
-    loop: function() {
+    loop: function () {
       if (this._stopped) {
         return;
       }
@@ -646,26 +663,26 @@
         this.publish(msg);
         // add back
         // if (this._wishes.length < PM.maxFeeds) {
-          this._wishes.push(msg);
+        this._wishes.push(msg);
         // }
       }
 
       setTimeout($.proxy(PM.board.loop, this), this.duration);
     },
-    publish: function(msg) {
+    publish: function (msg) {
       var cmt = {};
       // 根据每个人的账号生成专属的颜色
-      var color = str2rgb(msg.accountID+'').join(',');
+      var color = str2rgb(msg.accountID + '').join(',');
       // 字幕的节点内容
       cmt.text = '<span class="icon">' +
-        (msg.headImgUrl ? ('<img src="'+ msg.headImgUrl.replace(/\/0$/, '/96') +'">') : '') +
+        (msg.headImgUrl ? ('<img src="' + msg.headImgUrl.replace(/\/0$/, '/96') + '">') : '') +
         '</span><span class="text" style="background:rgba(' + color + ',0.4)">' +
-          (this.showName ? (msg.nickName + '：') : '') +
-          msg.msg +
+        (this.showName ? (msg.nickName + '：') : '') +
+        msg.msg +
         '</span>';
       cmt.mode = 1;
       cmt.size = 16;
-      cmt.dur = Math.floor(Math.random()*4000 + this.speed);
+      cmt.dur = Math.floor(Math.random() * 4000 + this.speed);
 
       this._cm.send(cmt);
     }
