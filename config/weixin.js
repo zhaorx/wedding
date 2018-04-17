0b4c13dc3fcd786d76c47d167defc0ea247f870a
zhaorx <547070625@qq.com>
2018-04-17 20:50:01 +0800

    正式公众号

--- config/weixin.js
+++ config/weixin.js
@@ -1,9 +1,11 @@
 module.exports.weixin = {
   // 微信公众号相关设置
   //测试号
-  appid: 'wx9db84d33fd2c2d88',
-  secret: 'd4624c36b6795d1d99dcf0547af5443d',
-  // redirectUri: 'http://139.196.126.164/home/resolve'
-  // redirectUri: 'http://chengchen.vicp.cc:34604/home/resolve'
-  redirectUri: 'http://zrx.ht-dep.club/home/resolve'
+  // appid: 'wx9db84d33fd2c2d88',
+  // secret: 'd4624c36b6795d1d99dcf0547af5443d',
+  // redirectUri: 'http://zrx.ht-dep.club/home/resolve'
+  //正式号
+  appid: 'wx0678787b5b20198a',
+  secret: '61cd4d0d5f2ee7faaf72ebf3881b2624',
+  redirectUri: 'http://www.vl8.top/home/resolve'
 };
